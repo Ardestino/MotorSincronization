7040bd25fcd76d7f815f031c1fde93e757635be6
Jose Ramon Navarro Marquez <8094349+Ardestino@users.noreply.github.com>
2025-08-01 17:40:03 -0600

    Updated pinout q1-q4

--- main/include/config.h
+++ main/include/config.h
@@ -5,17 +5,17 @@ const gpio_num_t Q1_DIR = GPIO_NUM_26; // Azul     // Amarillo
 const gpio_num_t Q1_STP = GPIO_NUM_27; // Morado   // Verde
 const gpio_num_t Q1_ENA = GPIO_NUM_14; // Gris     // Azul
 
-const gpio_num_t Q2_DIR = GPIO_NUM_21; // Azul
-const gpio_num_t Q2_STP = GPIO_NUM_19; // Morado
-const gpio_num_t Q2_ENA = GPIO_NUM_18; // Gris
+const gpio_num_t Q2_DIR = GPIO_NUM_32 ; // Azul
+const gpio_num_t Q2_STP = GPIO_NUM_33; // Morado
+const gpio_num_t Q2_ENA = GPIO_NUM_25; // Gris
 
-const gpio_num_t Q3_DIR = GPIO_NUM_5; // Azul
-const gpio_num_t Q3_STP = GPIO_NUM_17; // Morado
-const gpio_num_t Q3_ENA = GPIO_NUM_16; // Gris
+const gpio_num_t Q3_DIR = GPIO_NUM_21; // Azul
+const gpio_num_t Q3_STP = GPIO_NUM_19; // Morado
+const gpio_num_t Q3_ENA = GPIO_NUM_18; // Gris
 
-const gpio_num_t Q4_DIR = GPIO_NUM_32; // Azul
-const gpio_num_t Q4_STP = GPIO_NUM_33; // Morado
-const gpio_num_t Q4_ENA = GPIO_NUM_25; // Gris
+const gpio_num_t Q4_DIR = GPIO_NUM_5; // Azul
+const gpio_num_t Q4_STP = GPIO_NUM_17; // Morado
+const gpio_num_t Q4_ENA = GPIO_NUM_16; // Gris
 
 const gpio_num_t Q1_LSW = GPIO_NUM_23;
 const gpio_num_t Q2_LSW = GPIO_NUM_22;
